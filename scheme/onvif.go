440a000424bc42a2ee7606f2de6163b38b4eca46
George Palanjyan <jpalanjyan@gmail.com>
2017-11-28 00:54:43 +0300

    Types

---
+++ scheme/onvif.go
@@ -0,0 +1,19 @@
+package scheme
+
+//Types
+
+type TypeWorker interface {
+	GetProperties() []interface{}
+}
+
+type ContinuousMove struct {
+	ReferenceToken 	string
+	Pan, Tilt 		float64
+	Zoom 			float64
+	Timeout 		int //In seconds. Maybe need set more possibilities
+}
+
+
+func (t *ContinuousMove) GetProperties() []interface{} {
+	return []interface{}{t.ReferenceToken, t.Pan, t.Tilt, t.Zoom, t.Timeout}
+}
--- soap.go
+++ soap.go
@@ -1,10 +1,17 @@
 package goonvif
 
-import "github.com/achiku/xml"
+//import "encoding/xml"
+
+//type Imd struct {
+//	Anim string
+//}
 
 type Envelope struct {
-
-	Name xml.Name
-
-
+	//Name 		xml.Name 	`xml:"notice"`
+	Id 			int			`xml:"id,attr"`
+	FirstName 	string   	`xml:"name>first"`
+	LastName  	string   	`xml:"name>last"`
+	NameId		string		`xml:"jorj, mas"`
+	Age       	int      	`xml:"age"`
+	Comment 	string 		`xml:",comment"`
 }
