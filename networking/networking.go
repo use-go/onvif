df994b34ea3a37935e44f234d7863e5dddf18df3
yakovlevdmv <yakovlevdmv@gmail.com>
2018-04-10 22:22:50 +0300

    Merge conflicts

--- networking/networking.go
+++ networking/networking.go
@@ -3,30 +3,14 @@ package networking
 import (
 	"net/http"
 	"bytes"
-	"fmt"
 )
 
 func SendSoap(endpoint, message string) (*http.Response, error) {
-	fmt.Println(message)
 	httpClient := new(http.Client)
 
 	resp, err := httpClient.Post(endpoint, "application/soap+xml; charset=utf-8", bytes.NewBufferString(message))
 	if err != nil {
 		return resp, err
 	}
-
-	fmt.Println(resp.Header)
-
-	/*if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusBadRequest {
-		return "", errors.New("error: got HTTP response status " + strconv.Itoa(resp.StatusCode))
-	}*/
-	//b, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	return resp, err
-	//}
-	//fmt.Println(endpoint)
-	//fmt.Println(string(b))
-	//log.Println(resp.StatusCode)
-
 	return resp,nil
 }
