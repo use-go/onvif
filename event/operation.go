8b395f0f8a135648b1a7033fb527190c671f1610
GyeongHo Kim <gyeongho.dev@proton.me>
2026-04-17 16:04:27 +0900

    fix typo in SubscriptionPolicy

--- event/operation.go
+++ event/operation.go
@@ -61,7 +61,7 @@ type CreatePullPointSubscription struct {
 	XMLName                string                     `xml:"tev:CreatePullPointSubscription"`
 	Filter                 FilterType                 `xml:"tev:Filter"`
 	InitialTerminationTime AbsoluteOrRelativeTimeType `xml:"wsnt:InitialTerminationTime"`
-	SubscriptionPolicy     SubscriptionPolicy         `xml:"wsnt:sSubscriptionPolicy"`
+	SubscriptionPolicy     SubscriptionPolicy         `xml:"wsnt:SubscriptionPolicy"`
 }
 
 //CreatePullPointSubscriptionResponse action
