c02a6db0d5ef52302a99f7bf463b2f424d502a9a
GyeongHo Kim <me@gyeongho.dev>
2026-08-31 00:41:18 +0900

    fix: qualify CreatePullPointSubscription children in the tev namespace
    - the Event WSDL schema is elementFormDefault="qualified" with targetNamespace
      tev, so Filter, InitialTerminationTime and SubscriptionPolicy are tev-local
    - wsnt:InitialTerminationTime has no global declaration in b-2.xsd, so devices
      matched it against the trailing ##other wildcard and dropped it
    - make all three children optional pointers, as the schema declares minOccurs=0

--- event/operation.go
+++ event/operation.go
@@ -62,10 +62,10 @@ type UnsubscribeResponse struct { //http://docs.oasis-open.org/wsn/b-2.xsd
 
 //CreatePullPointSubscription action
 type CreatePullPointSubscription struct {
-	XMLName                string                     `xml:"tev:CreatePullPointSubscription"`
-	Filter                 FilterType                 `xml:"tev:Filter"`
-	InitialTerminationTime AbsoluteOrRelativeTimeType `xml:"wsnt:InitialTerminationTime"`
-	SubscriptionPolicy     SubscriptionPolicy         `xml:"wsnt:SubscriptionPolicy"`
+	XMLName                string                      `xml:"tev:CreatePullPointSubscription"`
+	Filter                 *FilterType                 `xml:"tev:Filter,omitempty"`
+	InitialTerminationTime *AbsoluteOrRelativeTimeType `xml:"tev:InitialTerminationTime,omitempty"`
+	SubscriptionPolicy     *SubscriptionPolicy         `xml:"tev:SubscriptionPolicy,omitempty"`
 }
 
 //CreatePullPointSubscriptionResponse action
